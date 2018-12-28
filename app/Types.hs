5805aaeb816dd4d481e343a734bbb8f58a5c24ad
Mamadou DIA <dia4onlineshoppings@gmail.com>
2018-12-28 20:52:05 +0100

    Test mode

--- app/Types.hs
+++ app/Types.hs
@@ -10,8 +10,9 @@ data Recipient = Person { fstName :: String
 type Location = String
 type Subject = String
 type Schedule =  UTCTime
-data Event = Event Subject Location Schedule [Attendee] Price Status Duration
+data Event = Event Subject Location Schedule [Recipient] Price Status Duration
 data Price = Maybe Int
 data Duration = Duration Int String
 data Status = Open | Close
+
 
